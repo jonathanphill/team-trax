2a31190e0a3eea3a2544ffc12d34ac30b0f09acb
jonathanphill <jonathanphill@gmail.com>
2023-05-24 10:25:53 -0400

    created the ability to search for an employee from the database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-
+import {BiUserPlus } from "react-icons/bi";
+import DropDown from './components/DropDown';
+import AddTimeOff from './components/AddTimeOff';
 function App() {
   return (
-    <div className="App">
-      <h1>team-trax</h1>
+    <div className="App wrapper">
+      <h1>team-TRAX</h1>
+      <p>manage your teams attendance</p>
+      <div className="search-bar-container">
+        <p>
+          <BiUserPlus id="user-icon" /> Select an Employee
+        </p>
+        <DropDown />
+      </div>
+      <div>
+        <AddTimeOff/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//   /* <ul>
+//         {employees.map((employee) => {
+//           return (
+//             <li key={employee.id}>
+//               <p>{employee.firstName +", "+employee.id}</p>
+//             </li>
+//           );
+//         })}
+//       </ul> */
